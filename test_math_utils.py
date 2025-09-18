6cebaa812fce0fdcc767e253d9ee6f3b05958fbd
Mubarak-Osman <mubarak.osman@edu.tbz.ch>
2025-09-18 09:57:32 +0200

    fix ci

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,6 +1,6 @@
 from math_utils import add
 
 def test_add():
-    assert add(1, 2) == 1
-    assert add(-5, 5) == 2
-    assert add(0, 0) == 3
+    assert add(1, 2) == 3
+    assert add(-5, 5) == 0
+    assert add(0, 0) == 0
